d4491fada9f253aa1d501af01b1320f4213944be
WiredrawnBoar <99906744+WiredrawnBoar@users.noreply.github.com>
2022-08-31 17:21:57 +0100

    Add files via upload

---
+++ JAVA.js
@@ -0,0 +1,41 @@
+function select_your_time() {
+    window.location = "HTML - Select Your Movie Choice.html";
+ }
+
+ let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+function booking(){
+alert ("You have succsesfully booked your movie")
+}
+
+function email(){
+alert ("You will now recieve our weekly newsletter via email")
+}
